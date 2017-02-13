3aa4d416e185234b264af004873582406388b8b1
mommoo <dv.mommoo@gmail.com>
2017-02-13 17:24:22 +0900

    MommooPanel 업데이트.

--- MinesWeeper/src/com/xogud02/test/Hello.java
+++ MinesWeeper/src/com/xogud02/test/Hello.java
@@ -1,8 +1,19 @@
 package com.xogud02.test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		
+		HashSet<Integer> set = new HashSet<Integer>();
+		Random r = new Random();
+		while(set.size()<20){
+			set.add(r.nextInt(50));
+		}
+		ArrayList<Integer> list = new ArrayList<>(set);
+		System.out.println(list);
 	}
+	
+	
 }
